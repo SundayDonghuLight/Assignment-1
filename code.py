b272be16857edacd70ffdceeb18387d0815b264d
SundayDonghuLight <38806778+SundayDonghuLight@users.noreply.github.com>
2018-04-30 00:57:09 +0800

    Create code.py
    讀入圖片

---
+++ code.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+PATH = "C:/Users/Mr-Fish/Desktop/Image_and_ImageData/"
+I = mpimg.imread(PATH + "I.png")
+E = mpimg.imread(PATH + "E.png")
+key1 = mpimg.imread(PATH + "key1.png")
+key2 = mpimg.imread(PATH + "key2.png")
